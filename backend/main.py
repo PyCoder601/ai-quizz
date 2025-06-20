48c45b0f8ffe98cc7a1a4c7fe1ee276ec6071954
romeomanoela <romeomanoela18@gmail.com>
2025-06-20 08:54:11 +0300

    deployement sur render: allowed_origins et logo

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,10 @@ app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://react-frontend-4ipn.onrender.com",
+        "http://localhost:5173/",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
