ab17e11bad059354e5f1a9b9b0f3845b753548b2
romeomanoela <romeomanoela18@gmail.com>
2025-06-20 08:39:50 +0300

    deployement sur render: mise à jour lien du backend

--- frontend/src/apis/api.ts
+++ frontend/src/apis/api.ts
@@ -6,7 +6,7 @@ import { loginUser } from '../features/userSlice.ts';
 const ACCESS_TOKEN = 'access_token';
 
 const api: AxiosInstance = axios.create({
-  baseURL: 'https://fastapi-backend-qbzg.onrender.com',
+  baseURL: 'https://fastapi-backend-qbzg.onrender.com/api',
   withCredentials: true,
 });
 
@@ -31,7 +31,7 @@ api.interceptors.response.use(
 
       try {
         const response = await axios.post(
-          `https://fastapi-backend-qbzg.onrender.com/refresh/`,
+          `https://fastapi-backend-qbzg.onrender.com/api/refresh/`,
           {},
           { withCredentials: true },
         );
