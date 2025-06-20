8dedff5390aabd9c7a45f097e49e1f784557c018
romeomanoela <romeomanoela18@gmail.com>
2025-06-20 21:21:06 +0300

    render: ping à chaque 15 min pour evider que render l'endorme

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,6 @@
 import os
+import asyncio
+import httpx
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -9,10 +11,21 @@ from backend.routes import auth, quiz
 from backend.database.models import User, Quiz
 from dotenv import load_dotenv
 
+from backend.database.db import engine
+
 load_dotenv()
 
 
-from backend.database.db import engine
+async def keep_alive():
+    while True:
+        try:
+            print("⏳ Sending keep-alive ping...")
+            async with httpx.AsyncClient() as client:
+                await client.get(os.getenv(os.getenv("BACKEND_API") + "/ping"))
+            print("✅ Ping sent.")
+        except Exception as e:
+            print(f"❌ Ping failed: {e}")
+        await asyncio.sleep(14 * 60)
 
 
 @asynccontextmanager
@@ -21,12 +34,14 @@ async def lifespan(app: FastAPI):
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
     print("Database started.")
+    asyncio.create_task(keep_alive())
     yield
     print("Closing app...")
 
 
 app = FastAPI(lifespan=lifespan)
 
+# Middleware CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[os.getenv("FRONTEND_URL")],
@@ -35,7 +50,12 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-print(os.getenv("FRONTEND_URL"))
-
+# Routes principales
 app.include_router(auth.router, prefix="/api")
 app.include_router(quiz.router, prefix="/api")
+
+
+# Route de ping
+@app.get("/api/ping")
+async def ping():
+    return {"message": "pong"}
