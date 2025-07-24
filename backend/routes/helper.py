b7ba53c55bbd2e4439d3f56a79cc18b75750f80e
romeomanoela <romeomanoela18@gmail.com>
2025-07-24 08:45:11 +0300

    fix: convert `reset_quota_if_needed` to async

--- backend/routes/helper.py
+++ backend/routes/helper.py
@@ -5,11 +5,11 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from backend.database.models import Quota
 
 
-def reset_quota_if_needed(db: AsyncSession, quota: Quota):
+async def reset_quota_if_needed(db: AsyncSession, quota: Quota):
     now = datetime.now()
     if now - quota.last_reset > timedelta(hours=5):
         quota.quota_remaining = 5
         quota.last_reset = now
-        db.commit()
-        db.refresh(quota)
+        await db.commit()
+        await db.refresh(quota)
     return quota
